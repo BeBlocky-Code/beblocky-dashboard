cc43efd746a19b109bfdefceca4feca7cf24fe87
Nahom Teguade <nahomteguade@gmail.com>
2026-08-08 10:32:19 +0300

    feat: add loading component for course editor with skeleton UI for improved user experience during data fetching

---
+++ app/(root)/courses/[id]/edit/loading.tsx
@@ -0,0 +1,9 @@
+import { CourseEditorPageSkeleton } from "@/components/courses/loading/course-edit-skeleton";
+
+export default function CourseEditLoading() {
+  return (
+    <div className="pt-2">
+      <CourseEditorPageSkeleton />
+    </div>
+  );
+}
